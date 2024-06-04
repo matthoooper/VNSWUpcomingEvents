f0a7ef20796cb5fe71939f6a323cc4ef2e7b59b3
matthoooper <matthewjhooper@outlook.com>
2024-06-04 13:02:17 +1000

    Commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   res.setHeader("X-Frame-Options", "DENY");
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self';"
+    "default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline';"
   );
   res.setHeader("X-Content-Type-Options", "nosniff");
   next();
